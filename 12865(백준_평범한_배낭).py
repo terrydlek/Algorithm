c36cfbe921b40e50c716c7f70d8edb134f5074bb
Jeuk Park <terryf6188@gmail.com>
2024-04-21 15:50:09 +0900

    algorithm

---
+++ 12865(백준_평범한_배낭).py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+n, k = map(int, input().split())
+
+dp = [[0] * (k + 1) for _ in range(n + 1)]
+bag = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(1, n + 1):
+    for j in range(1, k + 1):
+        if j >= bag[i - 1][0]:
+            dp[i][j] = max(bag[i - 1][1] + dp[i - 1][j - bag[i - 1][0]], dp[i - 1][j])
+        else:
+            dp[i][j] = dp[i - 1][j]
+
+print(dp[n][k])
